831703d48be3452b613bc30f744f592490216fdf
thespiker <fili_vw23@student.itstep.org>
2024-09-03 09:34:12 +0300

    Добавьте файлы проекта.

---
+++ DataAccess/Models/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Task
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public TaskStatus Status { get; set; }
+        public TaskPriority Priority { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+
+    public enum TaskStatus { Pending, InProgress, Completed }
+    public enum TaskPriority { Low, Medium, High }
+}
